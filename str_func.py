4ccbc5fa20c77717e6644bf3138091703d568a00
Roman Igoshin <igoshinromanchik@yandex.ru>
2024-05-22 13:54:10 +0300

    Разрешен конфликт с веткой develop, отклонены изменения другого разработчика

--- str_func.py
+++ str_func.py
@@ -1,12 +1,3 @@
-def to_uppercase(s: str) -> str:
-    """
-    Принимает строку и возвращает её со всеми заглавными буквами.
-
-    :param s: Исходная строка.
-    :return: Строка с заглавными буквами.
-    """
-    return s.upper()
-
 
 def capitalize_words(s: str) -> str:
     """
