b3c57d44d2d7e77335a49eff425444b0149a382a
Roman Igoshin <igoshinromanchik@yandex.ru>
2024-05-22 13:45:04 +0300

    Добавлена функция capitalize_words с docstring в str_func.py

--- str_func.py
+++ str_func.py
@@ -6,3 +6,13 @@ def to_uppercase(s: str) -> str:
     :return: Строка с заглавными буквами.
     """
     return s.upper()
+
+
+def capitalize_words(s: str) -> str:
+    """
+    Принимает строку и делает заглавными первые буквы каждого слова.
+
+    :param s: Исходная строка.
+    :return: Строка с заглавными первыми буквами каждого слова.
+    """
+    return s.title()
